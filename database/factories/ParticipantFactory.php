0f9d6a0dcbd00e9595df64efac05b57338f44f32
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-04 16:30:34 -0300

    feat: participant model and factory

---
+++ database/factories/ParticipantFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Participant>
+ */
+class ParticipantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'phone' => fake()->phoneNumber(),
+            'dozens' => json_encode([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+            'points' => fake()->numberBetween(0, 10),
+            'update_number' => fake()->numberBetween(1000, 9999),
+            'is_active' => true
+        ];
+    }
+}
