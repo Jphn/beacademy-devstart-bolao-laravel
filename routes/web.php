6dc6987275bd84b29df63fcb0d247432037955cb
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-04 16:29:46 -0300

    feat: index routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
 
-Route::get('/', function () {
+Route::get('/', [IndexController::class , 'getTablePage'])->name('index.table');
+Route::get('/volante/{id}', [IndexController::class , 'getWheelPage'])->name('index.wheel');
+
+Route::get('/about', function () {
     return view('welcome');
 });
