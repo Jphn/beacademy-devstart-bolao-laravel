fca54f9edb9d01c1a917596a6ff0b0764c0e8c66
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-04 23:39:57 -0300

    feat: sweepstake auto update route

---
+++ app/Http/Controllers/SweepstakeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Sweepstake;
+
+class SweepstakeController extends Controller
+{
+    public function __construct(Sweepstake $model)
+    {
+        $this->model = $model;
+    }
+
+    public function putLatestSweepstake()
+    {
+        $result = Http::withoutVerifying()->get('https://loteriascaixa-api.herokuapp.com/api/mega-sena/latest')->json(); // FIXME - Debug only.
+
+        $data = [
+            'id' => $result['concurso'],
+            'dozens' => json_encode($result['dezenas']),
+            'next_date' => date('Y-m-d', strtotime(str_replace('/', '-', $result['dataProxConcurso'])))
+        ];
+
+        if (!Sweepstake::find($data['id'])->exists())
+            $this->model->create($data);
+
+        return redirect()->route('admin.dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\SweepstakeController;
+use App\Http\Controllers\ParticipantController;
 
 /* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
 
 Route::get('/', [IndexController::class , 'getTablePage'])->name('index.table');
 Route::get('/volante/{id}', [IndexController::class , 'getWheelPage'])->name('index.wheel');
 
-
 Route::get('/login', [AdminController::class , 'getLoginPage'])->name('admin.login');
 Route::post('/login', [AdminController::class , 'postLogin'])->name('admin.login.post');
-
 Route::get('/dashboard', [AdminController::class , 'getDashboardPage'])->middleware('admin.login')->name('admin.dashboard');
 Route::get('/logout', [AdminController::class , 'getLogoutUser'])->name('admin.logout');
 
+Route::put('/sweepstakes', [SweepstakeController::class , 'putLatestSweepstake'])->middleware('admin.login')->name('sweepstakes.update');
 
 Route::get('/laravel', function () {
     return view('welcome');
