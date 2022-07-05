3ee6de886214bcf193e31a8a0018f636d592a4fe
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-04 21:04:52 -0300

    fix: added container class

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,28 +1,28 @@
 @extends('template.bootstrap')
 @section('title', 'Todos os participantes')
 @section('body')
-<section>
-	<h1>Tabela de participantes</h1>
-	
-	<table class="table">
-		<thead>
-			<tr>
-				<th>#</th>
-				<th>Nome</th>
-				<th>Pontos</th>
-				<th>Dezenas</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach($participants as $key => $participant)
-			<tr>
-				<th>{{ $key + 1 }}</th>
-				<td>{{ $participant->name }}</td>
-				<td>{{ $participant->points }}</td>
-				<td>{{ $participant->dozens }}</td>
-			</tr>
-			@endforeach
-		</tbody>
-	</table>
-</section>
+	<section class="container">
+		<h1>Tabela de participantes</h1>
+
+		<table class="table">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Nome</th>
+					<th>Pontos</th>
+					<th>Dezenas</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($participants as $key => $participant)
+					<tr>
+						<th>{{ $key + 1 }}</th>
+						<td>{{ $participant->name }}</td>
+						<td>{{ $participant->points }}</td>
+						<td>{{ $participant->dozens }}</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</section>
 @endsection
