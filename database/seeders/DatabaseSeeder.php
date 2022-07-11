68994eb0bb4a913bb5c39fbe473d9247923c1791
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-07-11 16:40:33 -0300

    `fix:` seeder will create a admin user by default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com'
+        ]);
 
-        \App\Models\Participant::factory(10)->create();
+        \App\Models\Participant::factory(30)->create();
 
     // \App\Models\User::factory()->create([
     //     'name' => 'Test User',
