91c08e8f9f01029fa0a9097a235b07681038bd14
João Pedro Holanda Neves <joaopedroholandaneves@gmail.com>
2022-08-03 00:27:34 -0300

    tests: SweepstakeControllerTest.php

---
+++ tests/Feature/app/Http/Controllers/SweepstakeControllerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature\app\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Participant;
+use App\Models\Sweepstake;
+use Illuminate\Support\Facades\Http;
+
+class SweepstakeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testAdminCanAutoUpdateAllParticipantsPoints()
+    {
+        // PREPARE
+        $result = Http::withoutVerifying()->get('https://loteriascaixa-api.herokuapp.com/api/mega-sena/latest')->json();
+        $user = User::factory()->create();
+        $participant = Participant::factory()->create();
+
+        // ACT
+        $this->actingAs($user);
+        $response = $this->put(route('sweepstakes.update'));
+
+        // ASSERT
+        $response->assertRedirect(route('admin.dashboard'));
+        $this->assertDatabaseCount(Participant::class , 1);
+        $this->assertDatabaseCount(Sweepstake::class , 1);
+        $this->assertDatabaseMissing(Participant::class , [
+            'update_number' => $participant->update_number
+        ]);
+        $this->assertDatabaseHas(Participant::class , [
+            'update_number' => $result['concurso']
+        ]);
+        $this->assertDatabaseHas(Sweepstake::class , [
+            'id' => $result['concurso'],
+        ]);
+    }
+}
